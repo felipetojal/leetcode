8fded62ab27e8ca2f036a8449bb5b0055dffd5fb
felipetojal <felipetojal07@gmail.com>
2026-05-06 22:51:20 -0300

    14-longestCommonPrefix is done.

--- 14-longestCommonPrefix/main.go
+++ 14-longestCommonPrefix/main.go
@@ -5,66 +5,39 @@ import (
 	"strings"
 )
 
-type occurence struct {
-	character string
-	count     int
-	positions []int
-}
-
 func longestCommonPrefix(strs []string) string {
-
-	prefixes := make([]string, 0)
-
-	for i := 0; i < len(strs); i++ {
-
+	
+	splitRoot := strings.Split(strs[0], "")
+	prefix := splitRoot
+	if len(prefix) == 0 {
+		return ""
 	}
 
-	return ""
-}
-
-func createStrOccurrences(str string) map[string]occurence {
-	m := make(map[string]occurence)
-	split := strings.Split(str, "")
-	for pos, char := range split {
-		o, ok := m[char]
-		if !ok {
-			m[char] = occurence{
-				character: char,
-				count:     1,
-				positions: []int{pos},
+	// Iterating over all strings
+	for i := 0; i < len(strs); i++ {
+		split := strings.Split(strs[i], "")
+		// Iterating over the characters
+		min := min(len(split), len(prefix))
+		prefix = prefix[:min]
+		for j := 0; j < min; j++ {
+			comparison := split[j] == prefix[j]
+			if !comparison {
+				prefix = prefix[:j]
+				break
 			}
-		} else {
-			o.count++
-			o.positions = append(o.positions, pos)
-			m[char] = o
+		}
+
+		if len(prefix) == 0 {
+			return ""
 		}
 	}
 
-	return m
-}
-
-/*
-Ideia: fazer por ordem sequencial. Teríamos O(n).
-
-Pega a primeira string, vê se ela tem algum prefixo em comum com a segunda.
-Se elas não tiverem, ja podemos finalizar a função pois não há nada em
-comum entre todas as strings.
-
-Se tiverem algo em comum, separamos somente a parte em comum e comparamos
-com a string seguinte, repetindo o processo até o final do slice.
-*/
-
-func printOccurenceMap(m map[string]occurence) {
-	for s, o := range m {
-		fmt.Println("--------")
-		fmt.Printf("string: %v\n", s)
-		fmt.Printf("occurence character: %v\n", o.character)
-		fmt.Printf("occurrence count: %v\n", o.count)
-		fmt.Printf("occurence positions: %v\n", o.positions)
-	}
+	return strings.Join(prefix, "")
 }
 
 func main() {
-	m := createStrOccurrences("paralelepipedo")
-	printOccurenceMap(m)
+	strs := []string{"ab", "a"}
+	result := longestCommonPrefix(strs)
+	fmt.Printf("Result: %v\n", result)
+
 }
